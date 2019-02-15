c29cd42a389ea54fd62e2db5de7d1bab71c97e6f
Louis <cantinieauxlouis@gmail.com>
2019-02-15 16:33:38 +0100

    Final version before release

--- script.js
+++ script.js
@@ -14,16 +14,6 @@ import 'carousel';
 document.getElementById("staticEmail").innerHTML = document.getElementById("exampleInputEmail1").value
 
 // Activate Carousel
-$("#myCarousel").carousel({
-
-});
-
-// Enable Carousel Indicators
-$(".item").click(function(){
-  $("#myCarousel").carousel(1);
-});
-
-// Enable Carousel Controls
-$(".left").click(function(){
-  $("#myCarousel").carousel("prev");
-});
+$('.carousel').carousel({
+  interval: 2000
+})
